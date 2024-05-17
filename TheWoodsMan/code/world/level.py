2d33a021a8b46c4f3a93e144ac208c66499e8d73
Krzysztof F <krzysztoff0423@gmail.com>
2024-05-17 22:51:51 +0200

    f

--- TheWoodsMan/code/world/level.py
+++
@@ -1,5 +0,0 @@
-from ursina import Entity, Plane, load_texture
-
-floor = Plane(texture=load_texture("graphics/textures/floor.jpg"))
-floor.scale = 100, 100
-player = Entity(model="graphics/models/character.obj", position=(0, 0, 0.5))
--- TheWoodsMan/config.py
+++
@@ -1,6 +0,0 @@
-from ursina import Ursina
-
-app = Ursina()
-
-if __name__ == "__main__":
-  app.run()
